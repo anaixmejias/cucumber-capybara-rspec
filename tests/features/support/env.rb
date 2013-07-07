74bcf075501057fb1aa6eadc47e3a43683648771
Anaix Mejias <anaixmejias@gmail.com>
2013-07-07 11:05:41 -0400

    added new gem poltergeist

--- tests/features/support/env.rb
+++ tests/features/support/env.rb
@@ -2,5 +2,15 @@
 require "selenium-webdriver"
 require "capybara/cucumber"
 require "rspec/expectations"
+require "capybara/poltergeist"
 
 Capybara.default_driver = :selenium
+
+
+Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, {debug: false})
+end
+
+Capybara.javascript_driver = :poltergeist
+
+
